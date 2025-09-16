fc7410b97780e65ac9ba760361c167c238d7f85d
osamamohamedr1 <osamarizk262@gmail.com>
2025-09-16 15:35:01 +0300

    test mangement cases

--- main.dart
+++ main.dart
@@ -9,16 +9,22 @@ void main() {
 
   manager.addNewOrder(
     OrderModel(
-      customerName: 'Ali',
-      drink: HibiscusTea(),
+      customerName: 'Osama',
+      drink: Shai(),
       specialInstructions: 'extra mint',
     ),
   );
 
-  manager.addNewOrder(OrderModel(customerName: 'Mona', drink: TurkishCoffee()));
-  manager.addNewOrder(OrderModel(customerName: 'Sara', drink: Shai()));
+  manager.addNewOrder(OrderModel(customerName: 'Omar', drink: TurkishCoffee()));
+  manager.addNewOrder(
+    OrderModel(
+      customerName: 'Ahmed',
+      drink: Shai(),
+      specialInstructions: "extra shuger, ya rais",
+    ),
+  );
 
-  print('--- Pending Orders ---');
+  print('\n*** Pending Orders ***');
   for (var o in manager.viewPendingOrders()) {
     print('${o.customerName} ordered ${o.drink.name}');
   }
@@ -27,11 +33,11 @@ void main() {
   var firstOrder = manager.viewPendingOrders().first;
   manager.completeOrder(firstOrder);
 
-  print('\n--- Pending After Completion ---');
+  print('\n*** Pending After Completion ***');
   for (var o in manager.viewPendingOrders()) {
     print('${o.customerName} ordered ${o.drink.name}');
   }
 
-  print('\n--- Sales Report ---');
+  print('\n*** Sales Report ***');
   print(manager.viewSalesReport());
 }
